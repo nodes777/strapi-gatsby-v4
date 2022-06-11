ab43a8723469e5bc29bf187bd76782a7a90aef36
nodes777 <nodes777@gmail.com>
2022-06-11 10:25:36 +1000

    runs, but breaking because components needed in gatsby-node are missing

---
+++ frontend/gatsby-config.ts
@@ -0,0 +1,84 @@
+import type { GatsbyConfig } from "gatsby";
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+console.log("Token:");
+console.log(process.env.STRAPI_TOKEN);
+
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `TayloredToTaylor`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: "gatsby-source-strapi",
+    options: {
+      apiURL: process.env.STRAPI_API_URL || "http://localhost:1337",
+      accessToken: process.env.STRAPI_TOKEN,
+      collectionTypes: [
+        {
+          singularName: "article",
+          queryParams: {
+            publicationState:
+              process.env.GATSBY_IS_PREVIEW === "true" ? "preview" : "live",
+            populate: {
+              cover: "*",
+              blocks: {
+                populate: "*",
+              },
+            },
+          },
+        },
+        {
+          singularName: "author",
+        },
+        {
+          singularName: "category",
+        },
+      ],
+      singleTypes: [
+        {
+          singularName: "about",
+          queryParams: {
+            populate: {
+              blocks: {
+                populate: "*",
+              },
+            },
+          },
+        },
+        {
+          singularName: "global",
+          queryParams: {
+            populate: {
+              favicon: "*",
+              defaultSeo: {
+                populate: "*",
+              },
+            },
+          },
+        },
+      ],
+    },
+  },]
+};
+
+export default config;
